0b197969e16b7a1fa02795fad3a22e416a0349fc
John Patrick <jpattrick538@gmail.com>
2023-12-09 16:12:15 +0300

    feat:update props

--- src/components/RestaurantInfo.tsx
+++ src/components/RestaurantInfo.tsx
@@ -5,93 +5,81 @@ import styled from 'styled-components/native';
 import star from '../../assets/star';
 import open from '../../assets/open';
 import { SvgXml } from 'react-native-svg';
+import { RestaurantInfo } from '../services/restaurants/restaurant.context';
 
-export type RestaurantInfoProps = {
-    name: string;
-    icon: string;
-    photo: string[];
-    address: string;
-    isOpenNow: boolean;
-    rating: (1 | 2 | 3 | 4 | 5)
-    isClosedTemp: boolean;
-    cousine: string
-}
 const Title = styled.Text`
-color:${(props:any)=>props.theme.colors.ui.primary};
-font-size:${(props:any)=>props.theme.fontSizes.title};
-font-weight:${(props:any)=>props.theme.fontWeights.bold};
-font-family:${(props:any)=>props.theme.fonts.heading};
+color:${(props: any) => props.theme.colors.ui.primary};
+font-size:${(props: any) => props.theme.fontSizes.title};
+font-weight:${(props: any) => props.theme.fontWeights.bold};
+font-family:${(props: any) => props.theme.fonts.heading};
 `
-const RestaurantCard = styled(Card)`
-background-color:${(props:any)=>props.theme.colors.bg.primary};
-padding:${(props:any)=>props.theme.sizes[0]};
-margin-bottom:${(props:any)=>props.theme.sizes[1]};
+
+const Icon = styled.Image`
+height:18px;
+width:18px
+`
+export const RestaurantCard = styled(Card)`
+background-color:${(props: any) => props.theme.colors.bg.primary};
+padding:${(props: any) => props.theme.sizes[0]};
+margin-bottom:${(props: any) => props.theme.sizes[1]};
 `
 const RestaurantCardCover = styled(Card.Cover)`
 background-color:#fff;
 `
 const Info = styled(View)`
-padding:${(props:any)=>props.theme.sizes[0]};
+padding:${(props: any) => props.theme.sizes[0]};
 `
 const Rating = styled(View)`
 flex-direction:row;
 `
 const RatingWrapper = styled(View)`
-padding:${(props:any)=>props.theme.sizes[0]} 0;
+padding:${(props: any) => props.theme.sizes[0]} 0;
 flex-direction:row;
 justify-content:space-between;
 align-items:center;
 `
 const Address = styled(Text)`
-font-family:${(props:any)=>props.theme.fonts.body};
-font-size:${(props:any)=>props.theme.fontSizes.caption};
+font-family:${(props: any) => props.theme.fonts.body};
+font-size:${(props: any) => props.theme.fontSizes.caption};
 `
 const IsClosed = styled(Text)`
-font-family:${(props:any)=>props.theme.fonts.body};
-font-size:${(props:any)=>props.theme.fontSizes.caption};
+font-family:${(props: any) => props.theme.fonts.body};
+font-size:${(props: any) => props.theme.fontSizes.caption};
 color:red;
 `
-export const RestaurantInfoCard: React.FC<RestaurantInfoProps> = ({
+export const RestaurantInfoCard: React.FC<Pick<RestaurantInfo, "name" | "icon" | "photos" | "vicinity" | "rating" | "opening_hours">> = ({
     name,
     icon,
-    photo,
-    address,
+    photos,
+    vicinity,
     rating,
-    cousine,
-    isOpenNow,
-    isClosedTemp
+    opening_hours
 }) => {
- 
-    const ratingStars = Array.from(new Array(Math.floor(rating)))
+    const ratingStars = Array.from(new Array(Math.floor(+rating)))
 
     return (
         <RestaurantCard
         >
-            {/* <Card.Title  title={name} subtitle={cousine} /> */}
-            <RestaurantCardCover source={{ uri: photo[0] }} />
+            <RestaurantCardCover source={{ uri: (photos[0].photo_reference != "") ? photos[0].photo_reference.length : 'https://media.cnn.com/api/v1/images/stellar/prod/230320152734-02-mexican-foods-birria.jpg?c=original&q=h_618,c_fill' }} />
             <Info>
-            <Title>{name}</Title>
-            <RatingWrapper>
-
-            <Rating>
-
-            {
-                ratingStars.map((_,i)=>{
-                    return <SvgXml key={i} xml={star} width={20} height={20}/>
-                })
-                
-            }
-            </Rating>
-            <Rating style={{gap:10}}>
-            {
-                (isOpenNow && !isClosedTemp) ? <SvgXml xml={open} width={20} height={20} /> : <IsClosed>TEMPORARILY CLOSED</IsClosed>
-            }
-             <SvgXml  xml={icon} width={20} height={20}/>
-            </Rating>
-            </RatingWrapper>
-            <Address>{address}</Address>
+                <Title>{name}</Title>
+                <RatingWrapper>
+                    <Rating>
+                        {
+                            ratingStars.map((_, i) => {
+                                return <SvgXml key={i} xml={star} width={20} height={20} />
+                            })
+                        }
+                    </Rating>
+                    <Rating style={{ gap: 10 }}>
+                        {
+                          opening_hours &&  (opening_hours.open_now) ? <SvgXml xml={open} width={20} height={20} /> : <IsClosed>TEMPORARILY CLOSED</IsClosed>
+                        }
+                        <Icon source={{ uri: icon }} />
+                    </Rating>
+                </RatingWrapper>
+                <Address>{vicinity}</Address>
             </Info>
-
         </RestaurantCard>
 
     )
