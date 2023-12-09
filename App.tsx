1bd29204d2906158df1687efd2573dda34baf50c
John Patrick <jpattrick538@gmail.com>
2023-12-09 16:12:56 +0300

    feat:add restaurant provider

--- App.tsx
+++ App.tsx
@@ -4,18 +4,43 @@ import RestaurantScreen from './src/screens/RestaurantScreen';
 import styled from 'styled-components/native';
 import { ThemeProvider } from 'styled-components/native';
 import { theme } from './src/utils/theme/theme';
-import {useFonts, Oswald_400Regular } from '@expo-google-fonts/oswald';
+import { useFonts, Oswald_400Regular } from '@expo-google-fonts/oswald';
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
 import TabNavigator from './src/components/TabNavigator';
+import { restaurantRequests } from './src/services/restaurants/restaurant.service';
+import { useContext, useEffect, useState } from 'react';
+import { RestaurantInfo, RestaurantProvider, restaurantContext } from './src/services/restaurants/restaurant.context';
 
 
 export default function App() {
 
-  const [oswaldReady] = useFonts({'Oswald_400Regular':Oswald_400Regular});
-  const [latoReady] = useFonts({'Lato_400Regular':Lato_400Regular});
+  const [oswaldReady] = useFonts({ 'Oswald_400Regular': Oswald_400Regular });
+  const [latoReady] = useFonts({ 'Lato_400Regular': Lato_400Regular });
+  const [restaurants, setRestaurants] = useState<RestaurantInfo[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState<any>(null)
+  
+  useEffect(() => {
+    const fetchRestaurants = async () => {
+      try {
+        setIsLoading(true);
+        const fetchedRestaurants = await restaurantRequests().then(data=>{
+          setTimeout(()=>setIsLoading(false),1500)
+          return data;
+        });
+        setRestaurants(fetchedRestaurants.results);        
+      } catch (e) {
+        console.log(e);
+        setIsLoading(false)
+        setIsError(e)
+        
+      };
+    }
+    fetchRestaurants()
+  }, [])
   
 
-  if (!oswaldReady || !latoReady) return null;
+  if (!oswaldReady || !latoReady ) return null;
 
   const SafeArea = styled(SafeAreaView)`
     flex: 1;
@@ -25,12 +50,14 @@ export default function App() {
    ${StatusBarReact.currentHeight && `padding-top:${StatusBarReact.currentHeight}px;`} 
   `
   return <>
-    <ThemeProvider theme={theme}>
-      <SafeArea >
-        <TabNavigator/>
-      </SafeArea>
-    </ThemeProvider>
-    <StatusBar style="auto" />
+    <RestaurantProvider value={{isLoading,isError,restaurants}}>
+      <ThemeProvider theme={theme}>
+        <SafeArea >
+          <TabNavigator />
+        </SafeArea>
+      </ThemeProvider>
+      <StatusBar style="auto" />
+    </RestaurantProvider>
 
   </>;
 }
