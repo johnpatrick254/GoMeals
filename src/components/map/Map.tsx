6e479bb359fbc3308120d617532db622db1b50ca
John Patrick <jpattrick538@gmail.com>
2023-12-30 19:37:32 +0300

    feat: add region props o map

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,13 +1,32 @@
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import styled from 'styled-components';
+import { RestaurantInfo } from '../../services/restaurants/restaurant.context';
 
-const MapCard= styled(MapView)`
+const MapCard = styled(MapView)`
 border-radius :20px;
 width: 100%;
 height: 100%;
 `
-export const Map:React.FC<{setMapLoaded:()=>void}> =({setMapLoaded})=>{
+type Region = {
+    latitude: number,
+    longitude: number,
+    latitudeDelta: number,
+    longitudeDelta: number,
+}
+export const Map: React.FC<{ restaurants: RestaurantInfo[], region: Region, setMapLoaded: () => void }> = ({ setMapLoaded, restaurants, region }) => {
     return <MapCard
-           onMapReady={() => setMapLoaded()}
-    />
+        onMapReady={() => setMapLoaded()}
+        region={region}
+
+    >
+        {restaurants.map(rest => <Marker
+            title={rest.name}
+            key={rest.name}
+            coordinate={{
+                longitude: rest.geometry.location.lng,
+                latitude: rest.geometry.location.lat
+            }}
+
+        />)}
+    </MapCard>
 }
