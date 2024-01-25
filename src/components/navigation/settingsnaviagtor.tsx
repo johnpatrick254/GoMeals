e58e26a95950f4e841f402117ee395249dee4f01
John Patrick <jpattrick538@gmail.com>
2024-01-26 00:12:37 +0300

    feat:setup setting screen

--- src/components/navigation/settingsnaviagtor.tsx
+++ src/components/navigation/settingsnaviagtor.tsx
@@ -1,6 +1,7 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import SettingScreen from "../../screens/SettingScreen";
 import { FavoriteScreen } from "../../screens/FavouritesScreen";
+import CameraScreen from "../../screens/CameraScreen";
 
 const SettingsStack = createStackNavigator();
 
@@ -16,6 +17,11 @@ export const SettingsNavigator: React.FC = () => {
         <SettingsStack.Screen
             component={FavoriteScreen}
             name='Favourites'
+            options={{ headerShown: true }}
+        />
+        <SettingsStack.Screen
+            component={CameraScreen}
+            name='camera'
             options={{ headerShown: true }}
         />
 
--- src/screens/SettingScreen.tsx
+++ src/screens/SettingScreen.tsx
@@ -1,16 +1,19 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { AuthContext } from '../services/auth/auth.context'
 import { getAuth } from 'firebase/auth'
-import { NavigationProp } from "@react-navigation/native";
+import { NavigationProp,useFocusEffect } from "@react-navigation/native";
 import { Avatar, Button, List } from 'react-native-paper'
 import styled from 'styled-components'
 import { colors } from '../utils/styles/colors'
 import { FadeInView } from '../components/shared/FadeInView'
+import { TouchableOpacity } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const SettingItem = styled(List.Item)`
-padding:${props => props.theme.space[3]}
+padding:${props => props.theme.space[3]};
+fontweight:bold;
 `
 const AvatarContainer = styled(View)`
 display:flex;
@@ -20,18 +23,32 @@ padding:${props => props.theme.space[3]}
 `
 const SettingScreen: React.FC<{ navigation: NavigationProp<any, any> }> = ({ navigation }) => {
   const { user } = useContext(AuthContext);
+  const [photo, setPhoto] = useState<string | null>(null)
   const logout = () => {
     const auth = getAuth()
     auth.signOut();
   }
-
-
+  const getProfilePic = async (currentUser:any) => {
+    const photoURI = await AsyncStorage.getItem(`profile-${currentUser.id}`)
+    setPhoto(photoURI);
+  }
+  useFocusEffect(() => {
+    getProfilePic(user);
+  })
 
   return (
     <SafeAreaView>
       <FadeInView>
         <AvatarContainer>
-          <Avatar.Icon size={180} icon={'human'} color='orange' style={{ backgroundColor: colors.bg.primary }} />
+          <TouchableOpacity onPress={() => navigation.navigate("camera")}>
+            {
+              photo
+                ?
+                <Avatar.Image size={150} source={{ uri: photo }} style={{ backgroundColor: colors.bg.primary }} />
+                :
+                <Avatar.Icon size={150} icon={'account-edit'} color='orange' style={{ backgroundColor: colors.bg.primary }} />
+            }
+          </TouchableOpacity>
           <Text style={{ fontWeight: 'bold' }} >{user.email.toString()}</Text>
         </AvatarContainer>
         <List.Section>
